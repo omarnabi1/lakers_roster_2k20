6adf83ffe93cabb16cb1d8acdf4b4a3a764ca002
Omar Nabi <onabi20@gmail.com>
2020-05-10 21:20:14 +0000

    Working on Bios

--- lib/lakers_roster_2k20/cli.rb
+++ lib/lakers_roster_2k20/cli.rb
@@ -19,15 +19,16 @@ class LakersRoster2k20::CLI
   
   def get_user_picked_player
     chosen_player = gets.strip.to_i
-    show_attributes_for(chosen_player) if valid_input(chosen_player.to_i, @players)
+    show_bio_for(chosen_player) if valid_input(chosen_player.to_i, @players)
   end
 
   def valid_input(input, data)
     input.to_i <= data.length && input.to_i > 0 
   end
   
-  def show_attributes_for(chosen_player)
+  def show_bio_for(chosen_player)
     player = @players[chosen_player - 1]
+    bio = player.bio 
     puts "Here is more about your favorite Laker, #{player.name}!"
   end
 end
--- lib/lakers_roster_2k20/player.rb
+++ lib/lakers_roster_2k20/player.rb
@@ -1,16 +1,24 @@
 class LakersRoster2k20::Player
 @@all = []
-attr_accessor :name 
+attr_accessor :name, :events
 
   def initialize(name) 
     @name = name 
+    @bio = []
     save
   end
   
   def self.all 
-  LakersRoster2k20::Scraper.scrape_players if @@all.empty?
-  @@all 
+    LakersRoster2k20::Scraper.scrape_players if @@all.empty?
+   @@all 
   end
+  
+  def bio
+    binding.pry
+    LakersRoster2k20::Scraper.scrape_bio(self) if @@bio.empty?
+    @bio 
+  end
+    
   
   def save
     @@all << self
